e7dc82bbab98dace98d13d639b8c1ff469392187
sudiptarana <sudiptarana00@gmail.com>
2019-10-10 10:27:10 +0530

    updated

---
+++ weather/dbconfig.php
@@ -0,0 +1,11 @@
+<?php
+/****************************************************/
+/************** iot home autometion *************/
+/****************************************************/ 
+
+define('DB_USER', "id10786212_iot");     // Your database user name
+define('DB_PASSWORD', "autohomeiot");			// Your database password (mention your db password here)
+define('DB_DATABASE', "id10786212_iot"); // Your database name
+define('DB_SERVER', "localhost");			// db server (Mostly will be 'local' host)
+
+?>
